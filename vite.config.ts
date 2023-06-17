8ee78f1f8cbc97c8f7a0cb3b316aa7bcb856e2e7
何家祥 <hjx@youchai.me>
2023-06-17 21:59:55 +0800

    addAction

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/JellyfishTuner/',
+  base:'/JellyfishTunner/',
   plugins: [react()],
   resolve: {
     alias: [
