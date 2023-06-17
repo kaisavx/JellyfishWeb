3463c5b1db8da8a34271617fefec62de56c0aa7f
何家祥 <hjx@youchai.me>
2023-06-17 18:13:26 +0800

    addAction

--- merge-master.sh
+++ merge-master.sh
@@ -3,9 +3,4 @@
 current_branch=`git rev-parse --abbrev-ref HEAD` &&
 echo "当前： $current_branch"
 git push --set-upstream origin $current_branch &&
-git pull &&
-echo "Merge branch '${current_branch}' into 'master'" &&
-git merge ${current_branch} develop --no-ff -m "Merge branch '${current_branch}' into 'develop'" &&
-git push &&
-echo "OK" &&
-git checkout ${current_branch}
+echo "Merge branch '${current_branch}' into 'master'"
