b1b57600e82fdf2c74168712e5c65f8f8d29b6ff
何家祥 <hjx@youchai.me>
2023-07-01 18:03:31 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
 import './App.css'
-import React, { Suspense, useEffect } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Tuner from '@src/pages/Tuner'
 
@@ -9,7 +7,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/JellyfishTuner/" element={<Tuner />} />
+          <Route path="/Jellyfish/" element={<Tuner />} />
         </Routes>
       </BrowserRouter>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/JellyfishTuner/',
+  base:'/Jellyfish/',
   plugins: [react()],
   resolve: {
     alias: [
